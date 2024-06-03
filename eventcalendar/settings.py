860c54d40fb534498ba4b0b51c8a1b19f8917774
Ivan <Dudasmit@bigmir.net>
2024-06-03 15:21:45 +0200

    2

--- eventcalendar/settings.py
+++ eventcalendar/settings.py
@@ -40,7 +40,9 @@ CSRF_TRUSTED_ORIGINS = ['http://.*',
                         'https://agenda.sepkoeriers.nl',
                         'http://agenda.sepkoeriers.nl',
                         'https://agenda.sepkoeriers.nl/accounts/profile/',
-                        'http://127.0.0.1:8000']
+                        'http://127.0.0.1:8000',
+                        'http://34.171.209.171',
+                        ]
 
 
 
