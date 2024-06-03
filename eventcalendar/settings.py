a3d325b17831df4decb56db5a2005d3e30b1586d
Ivan <Dudasmit@bigmir.net>
2024-06-03 11:54:19 +0200

    3

--- eventcalendar/settings.py
+++ eventcalendar/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-_q9&b%395)=(pebed(8ci_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', False)
 #DEBUG = True
+#print(DEBUG)
 
 ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS.append("eventcalendar.herokuapp.com")
@@ -38,9 +39,8 @@ ALLOWED_HOSTS.append("agenda.sepkoeriers.nl")
 
 CSRF_TRUSTED_ORIGINS = ['http://.*', 
                         'https://agenda.sepkoeriers.nl',
-                        'http://agenda.sepkoeriers.nl',
-                        'https://agenda.sepkoeriers.nl/accounts/profile/',
-                        'http://127.0.0.1:8000']
+                        'http://agenda.sepkoeriers.nl'
+                        ]
 
 
 
