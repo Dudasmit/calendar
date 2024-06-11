1abc3a9e5dc25fa977aa87ec0d9ad290436a6c41
Ivan <Dudasmit@bigmir.net>
2024-06-11 14:17:13 +0200

    3

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from core.models import Apointments, Winkel, TypeEvent
 
 # Register your models here.
-admin.site.register(Apointments)
+#admin.site.register(Apointments)
 admin.site.register(Winkel)
 admin.site.register(TypeEvent)
+
+
+@admin.register(Apointments)
+class CalendarAdmin(admin.ModelAdmin):
+    list_filter  = ["winkel","start_time"]
--- sepautoservice/admin.py
+++ sepautoservice/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import  Service_Apointments
 
 # Register your models here.
-admin.site.register(Service_Apointments)
+#admin.site.register(Service_Apointments)
 
+@admin.register(Service_Apointments)
+class CalendarAdmin(admin.ModelAdmin):
+    list_filter  = ["naam", "datum"]
